14eff796e99847a5db7e5b2ab5f9a8697aa7e8c4
Maddie Weber <maddieweber7@gmail.com>
2023-10-31 10:50:08 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+};
+
+
+const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  };
+
+// function wrapAdjective() {
+//     return function () {
+//             const word = 'special';
+//             return `You are ${word}`;
+//             return function 
+    
+
+//     }
+// }
+
+function wrapAdjective(visualFlair = "*", adjective = "special") {
+    return function(adjective = 'special') {
+      return `You are ${visualFlair}${adjective}${visualFlair}!`;
+    };
+  }
+
+  wrapAdjective("%")("a dedicated programmer");
